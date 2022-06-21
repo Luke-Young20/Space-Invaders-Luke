302039a4ff74930c5934e5702dd17343bf6a5590
Luke-Young20 <lukegyoung1@gmail.com>
2022-06-21 18:42:13 +0100

    Enemies spawning like how they should

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
-
+    [SerializeField] float rowCount = 12;
+    [SerializeField] float columnCount = 12;
+    float xSpawnStart = -8;
+    float ySpawnStart = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnEnemies", 0, 1);
+        SpawnEnemies();
+        //InvokeRepeating("SpawnEnemies", 0, 1);
     }
 
     private void SpawnEnemies()
@@ -22,8 +26,15 @@ public class EnemySpawner : MonoBehaviour
     public void SpawnEnemy()
 
     {
-        Vector2 location = new Vector2(-8,1.5f);
-        GameObject Enemy = Instantiate(Resources.Load("Enemy"), location, Quaternion.identity, this.transform) as GameObject;
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
 
+                Vector2 location = new Vector2(xSpawnStart + i, ySpawnStart + j);
+                GameObject Enemy = Instantiate(Resources.Load("Enemy"), location, Quaternion.identity, this.transform) as GameObject;
+
+            }
+        }
     }
 }
